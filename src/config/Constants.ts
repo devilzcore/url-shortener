3966024922c79f3293618f69dc92ab51accfffa5
devilzcore <aldosilva1@live.com>
2022-07-12 19:40:11 -0300

    Add docker-compose

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,6 @@
 export const config = {
   API_URL: "http://localhost:5000",
   MONGO_CONNECTION: 
-    "mongodb://localhost:27017/url-shortener"
+    // "mongodb://localhost:27017/url-shortener"
+    "mongodb://root:root@mongo:27017/"
 }
